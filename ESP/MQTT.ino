50f96e12029249cccb9d10b575593c0ff9b6b127
PSS <67224508+PSS101@users.noreply.github.com>
2025-09-03 10:23:12 +0530

    Add files via upload

--- ESP/MQTT.ino
+++ ESP/MQTT.ino
@@ -1,10 +1,8 @@
-#include <ESP8266WiFi.h>
+#include<WiFi.h>
 #include <PubSubClient.h>
 #include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#define DHTPIN D6   
-#define DHTTYPE    DHT22 
+#include <DHT22.h>
+#define DHTPIN 6    
 const char* ssid = "";
 const char* password =  "";
 const char* mqttServer = "192.168.1.8";
@@ -12,9 +10,9 @@ const int mqttPort = 1883;
 WiFiClient espClient;
 const char* mqttClientName = "ESP32-Client-LivingRoom";
 PubSubClient client(espClient);
-long lastReconnectAttempt = 0;
+long lastReconnectAttempt = 0,t=0,t_prev=0;
 int alert;
-
+DHT22 dht22(DHTPIN);
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
@@ -26,29 +24,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(message);
   if(strcmp(topic,"rpi/test/light")==0){
   if (strcmp(message, "on") == 0) {
-    digitalWrite(D2,LOW);
+    digitalWrite(7,LOW);
   }
   else{
-    digitalWrite(D2,HIGH);
+    digitalWrite(7,HIGH);
   }
   }
   else if(strcmp(topic,"rpi/test/bed")==0){
     if (strcmp(message, "on") == 0) {
-    digitalWrite(D5,LOW);
+    digitalWrite(5,LOW);
   }
   else{
-    digitalWrite(D5,HIGH);
+    digitalWrite(5,HIGH);
   }
   }
    else if(strcmp(topic,"rpi/alert")==0){
     if (strcmp(message, "check") == 0) {
-  if(alert==1){
-     client.publish("esp/alert","alert");
-     Serial.println("alert");
-  }
-  else{
-client.publish("esp/alert","");
-  }
+ 
     }
     if (strcmp(message, "reset") == 0) {
       alert = 0;
@@ -82,7 +74,7 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
-  WiFi.setOutputPower(19.25);
+
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
@@ -94,11 +86,11 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   delay(500); 
-  pinMode(D2,OUTPUT);
-  pinMode(D5,OUTPUT);
-  pinMode(D6,INPUT);
-  digitalWrite(D2,HIGH);
-  digitalWrite(D5,HIGH);
+  pinMode(2,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(1,INPUT);
+  digitalWrite(2,HIGH);
+  digitalWrite(7,HIGH);
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
  
@@ -106,7 +98,7 @@ void setup() {
 }
 
 void loop() {
-  int x = digitalRead(D6);
+  int x = digitalRead(1);
   alert = x==0?1:alert;
   if (!client.connected()) {
     long now = millis();
@@ -117,6 +109,25 @@ void loop() {
     }
   } else {
     client.loop();
+  }
+
+  t = millis();
+  float t = dht22.getTemperature();
+  float h = dht22.getHumidity();
+  if(t-t_prev>10000){
+    if(alert==1){
+     client.publish("esp/alert","alert");
+     Serial.println("alert");
+    }
+    else{
+        client.publish("esp/alert","");
+    }
+    if(!t.isnan() && !h.isnan()){
+      String mssg = String(t)+','+String(h)
+      char* buf[sizeof(mssg)];
+      mssg.toCharArray(buf, sizeof(buf));
+      client.subscribe("esp/weather",buf);
+    }
   }
   
 }
